23be53e97b90774d44fe129ba6dba1d77e7a2738
manoj1995madushanka <manoj1995madushanka@users.noreply.github.com>
2026-07-13 01:29:58 +0530

    configs

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -1,10 +1,7 @@
 package com.example.accounts.controller;
 
 import com.example.accounts.constants.AccountConstants;
-import com.example.accounts.dto.AccountsDto;
-import com.example.accounts.dto.CustomerDto;
-import com.example.accounts.dto.ErrorResponseDto;
-import com.example.accounts.dto.ResponseDto;
+import com.example.accounts.dto.*;
 import com.example.accounts.service.IAccountService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,8 +11,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -29,10 +28,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController // combination of @ResponseBody and @Controller
 @RequestMapping(value = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AccountsController {
 
-    private IAccountService accountService;
+    private final IAccountService accountService;
+
+    private final AccountsContactInfo accountsContactInfo;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
 
     @Operation(summary = "Create a new account", description = "This API creates a new account")
     @ApiResponse(responseCode = "201", description = "Account created successfully")
@@ -82,5 +90,26 @@ public class AccountsController {
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto(AccountConstants.STATUS_500, AccountConstants.MESSAGE_500));
         }
+    }
+
+    @Operation(summary = "Get build information", description = "Get build information")
+    @ApiResponse(responseCode = "200", description = "Account MS build version fetched successfully")
+    @GetMapping("/version")
+    public ResponseEntity<String> getVersion() {
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+
+    @Operation(summary = "Get java home information", description = "Get build information")
+    @ApiResponse(responseCode = "200", description = "Account MS build version fetched successfully")
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @Operation(summary = "Get contact information", description = "Get contact information")
+    @ApiResponse(responseCode = "200", description = "Account MS contact information fetched successfully")
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfo> getContactInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(accountsContactInfo);
     }
 }
