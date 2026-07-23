bbf57238a1eb3e090f169965245d8dd99b7ee6ad
manoj1995madushanka <manoj1995madushanka@users.noreply.github.com>
2026-07-23 13:00:19 +0530

    Config server integration

--- cards/src/main/java/com/eazybytes/cards/CardsApplication.java
+++ cards/src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -7,10 +7,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@ConfigurationPropertiesScan
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Cards microservice REST API Documentation",
